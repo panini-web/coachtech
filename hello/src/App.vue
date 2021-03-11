a7a5c7ce4f76c83f182ea4d0685f34c3dc7f1b24
パニーニ <jerrytom0610@gmail.com>
2021-03-11 19:00:23 +0900

    first-commit

---
+++ hello/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Header :parentText="sendText"></Header>
+    <p>App.vue</p>
+   </div>
+  </template>
+    
+<script> 
+import Header from "./components/Header";
+export default{
+ data(){
+   return{
+     sendText:"親コンポーネントのデータ"
+   };
+ },
+ components:{
+   Header
+ }
+};
+</script>
+
+<style>
+
+</style>
---
+++ hello/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="header">
+   <p>{{parentText}}</p>
+  </div>
+</template>
+<script>
+export default{
+  props:["parentText"]
+};
+<div id="side__menu">
+
+</script>
+
+<style scoped>
+ul{
+  display: flex;
+}
+li{
+  margin-right: 50px;
+}
+</style>
